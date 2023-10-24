5522411e55e3c273113c9c395d6eeff3768ced59
XOMRKOB <ru89050995432@yandex.ru>
2023-10-24 14:18:09 +0600

    add tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {Login} from "./pages/Login.tsx";
+import {Homepage} from "./pages/Homepage.tsx";
 
 const router = createBrowserRouter([
     {
-      path: '/',
+      path: '/login',
       element: <Login/>
     },
+  {
+    path: '/',
+    element: <Homepage />
+  }
   ],{
     basename: "/vite-example"
   }
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,9 @@
+export const Homepage = () => {
+  return (
+    <div className="container mx-auto">
+    <h1 className="text-3xl font-bold">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
